59f63b25847a50ae43dd5e4631567c0ade26bb40
bwalenz <bwalenz@a6c2d965-a440-0410-b3e7-8009764c6a45>
2009-02-24 04:10:09 +0000

    Added a JUnit test case for the Exc class.
    git-svn-id: https://jwordnet.svn.sourceforge.net/svnroot/jwordnet/trunk/jwnl@36 a6c2d965-a440-0410-b3e7-8009764c6a45

---
+++ test/net/didion/jwnl/data/TestExc.java
@@ -0,0 +1,71 @@
+/**
+ * 
+ */
+package net.didion.jwnl.data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import junit.framework.TestCase;
+
+/**
+ * @author bwalenz
+ *
+ */
+public class TestExc extends TestCase {
+
+	/** The private test object. */
+	private Exc testObj;
+	
+	/** Lemma representing this exception. */ 
+	private String lemma = "alam";
+	
+	/** 
+	 * List of exceptions from the lemma.
+	 */
+	private List exceptions;
+	
+	/**
+	 * First notional exception. 
+	 */
+	private String exc1 = "exc1";
+	
+	/* (non-Javadoc)
+	 * @see junit.framework.TestCase#setUp()
+	 */
+	protected void setUp() throws Exception {
+		exceptions = new ArrayList();
+		exceptions.add(exc1);
+		exceptions.add("exc2");
+		testObj = new Exc(POS.NOUN, lemma, exceptions); 
+	}
+
+	/**
+	 * Test method for {@link net.didion.jwnl.data.Exc#getPOS()}.
+	 */
+	public void testGetPOS() {
+		assertTrue(testObj.getPOS().equals(POS.NOUN));
+	}
+
+	/**
+	 * Test method for {@link net.didion.jwnl.data.Exc#getLemma()}.
+	 */
+	public void testGetLemma() {
+		assertTrue(testObj.getLemma().equals(lemma));
+	}
+
+	/** 
+	 * Test method for {@link net.didion.jwnl.data.Exc#getException()}.
+	 */
+	public void testGetException() {
+		assertTrue(testObj.getException(0).equals(exc1));
+	}
+
+	/**
+	 * Test method for {@link net.didion.jwnl.data.Exc#getExceptionsSize()}.
+	 */
+	public void testGetExceptionsSize() {
+		assertTrue(testObj.getExceptionsSize() == 2);
+	}
+
+}
