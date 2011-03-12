ffb6650fc99214ab6299743b3eb60355050e6521
Aliaksandr Autayeu <avtaev@gmail.com>
2011-03-12 17:44:43 +0100

    fixed Using Empty Result Set occur when using Database Dictionary - ID: 1282488
    http://sourceforge.net/tracker/?func=detail&aid=1282488&group_id=33824&atid=409470

--- extjwnl/src/main/java/net/sf/extjwnl/princeton/data/AbstractPrincetonDatabaseDictionaryElementFactory.java
+++ extjwnl/src/main/java/net/sf/extjwnl/princeton/data/AbstractPrincetonDatabaseDictionaryElementFactory.java
@@ -33,9 +33,9 @@ public abstract class AbstractPrincetonDatabaseDictionaryElementFactory extends 
         }
 
         long[] offsetArray = new long[offsets.size()];
-        Iterator itr = offsets.iterator();
+        Iterator<Long> itr = offsets.iterator();
         for (int i = 0; itr.hasNext(); i++) {
-            offsetArray[i] = (Long) itr.next();
+            offsetArray[i] = itr.next();
         }
 
         return new IndexWord(dictionary, lemma, pos, offsetArray);
@@ -43,48 +43,51 @@ public abstract class AbstractPrincetonDatabaseDictionaryElementFactory extends 
 
     public Synset createSynset(POS pos, long offset, ResultSet synsets, ResultSet words, ResultSet pointers, ResultSet verbFrames)
             throws SQLException, JWNLException {
-        synsets.next();
-        Synset synset = new Synset(dictionary, pos, offset);
-        boolean isAdjectiveCluster = synsets.getBoolean(1);
-        synset.setIsAdjectiveCluster(isAdjectiveCluster);
+        if (synsets.next()) {
+            Synset synset = new Synset(dictionary, pos, offset);
+            boolean isAdjectiveCluster = synsets.getBoolean(1);
+            synset.setIsAdjectiveCluster(isAdjectiveCluster);
 
-        String gloss = synsets.getString(2);
-        synset.setGloss(gloss);
+            String gloss = synsets.getString(2);
+            synset.setGloss(gloss);
 
-        while (words.next()) {
-            String lemma = words.getString(1);
-            int index = words.getInt(2);
-            Word word = createWord(synset, index, lemma);
-            word.setUseCount(words.getInt(3));
-            synset.getWords().add(word);
-        }
-
-        while (pointers.next()) {
-            PointerType type = PointerType.getPointerTypeForKey(pointers.getString(1));
-            long targetOffset = pointers.getLong(2);
-            POS targetPOS = POS.getPOSForKey(pointers.getString(3));
-            //int sourceIndex = pointers.getInt(4);
-            int targetIndex = pointers.getInt(5);
-            synset.getPointers().add(new Pointer(synset, type, targetPOS, targetOffset, targetIndex));
-        }
-
-        if (POS.VERB.equals(pos)) {
-            BitSet vFrames = new BitSet();
-            while (verbFrames.next()) {
-                int frameNumber = verbFrames.getInt(1);
-                int wordIndex = verbFrames.getInt(2);
-                if (wordIndex > 0) {
-                    ((MutableVerb) synset.getWords().get(wordIndex - 1)).setVerbFrameFlag(frameNumber);
-                } else {
-                    for (Word w : synset.getWords()) {
-                        ((MutableVerb) w).setVerbFrameFlag(frameNumber);
-                    }
-                    vFrames.set(frameNumber);
-                }
+            while (words.next()) {
+                String lemma = words.getString(1);
+                int index = words.getInt(2);
+                Word word = createWord(synset, index, lemma);
+                word.setUseCount(words.getInt(3));
+                synset.getWords().add(word);
             }
-            synset.setVerbFrameFlags(vFrames);
+
+            while (pointers.next()) {
+                PointerType type = PointerType.getPointerTypeForKey(pointers.getString(1));
+                long targetOffset = pointers.getLong(2);
+                POS targetPOS = POS.getPOSForKey(pointers.getString(3));
+                //int sourceIndex = pointers.getInt(4);
+                int targetIndex = pointers.getInt(5);
+                synset.getPointers().add(new Pointer(synset, type, targetPOS, targetOffset, targetIndex));
+            }
+
+            if (POS.VERB.equals(pos)) {
+                BitSet vFrames = new BitSet();
+                while (verbFrames.next()) {
+                    int frameNumber = verbFrames.getInt(1);
+                    int wordIndex = verbFrames.getInt(2);
+                    if (wordIndex > 0) {
+                        ((MutableVerb) synset.getWords().get(wordIndex - 1)).setVerbFrameFlag(frameNumber);
+                    } else {
+                        for (Word w : synset.getWords()) {
+                            ((MutableVerb) w).setVerbFrameFlag(frameNumber);
+                        }
+                        vFrames.set(frameNumber);
+                    }
+                }
+                synset.setVerbFrameFlags(vFrames);
+            }
+            return synset;
+        } else {
+            return null;
         }
-        return synset;
     }
 
     public Exc createExc(POS pos, String derivation, ResultSet rs) throws SQLException, JWNLException {
