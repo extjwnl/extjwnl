6697d2e9c58ae5c085e5f04679f6c2d4cfcf3b6e
Aliaksandr Autayeu <aliaksandr@autayeu.com>
2014-01-11 12:44:10 +0100

    removed unused code

--- extjwnl/src/main/java/net/sf/extjwnl/data/LexFileIdFileNameMap.java
+++ extjwnl/src/main/java/net/sf/extjwnl/data/LexFileIdFileNameMap.java
@@ -8,7 +8,7 @@ import java.util.*;
  * @author Brett Walenz <bwalenz@users.sourceforge.net>
  * @author <a rel="author" href="http://autayeu.com/">Aliaksandr Autayeu</a>
  */
-public abstract class LexFileIdFileNameMap implements Map<Long, String> {
+public abstract class LexFileIdFileNameMap {
 
     /**
      * A mapping of id's to files.
@@ -73,65 +73,5 @@ public abstract class LexFileIdFileNameMap implements Map<Long, String> {
 
     public static Map<Long, String> getMap() {
         return lexFileIdLexFileName;
-    }
-
-    public int size() {
-        return lexFileIdLexFileName.size();
-    }
-
-    public boolean equals(Object o) {
-        return lexFileIdLexFileName.equals(o);
-    }
-
-    public int hashCode() {
-        return lexFileIdLexFileName.hashCode();
-    }
-
-    public String toString() {
-        return lexFileIdLexFileName.toString();
-    }
-
-    public boolean isEmpty() {
-        return lexFileIdLexFileName.isEmpty();
-    }
-
-    public String get(Object key) {
-        return lexFileIdLexFileName.get(key);
-    }
-
-    public boolean containsKey(Object key) {
-        return lexFileIdLexFileName.containsKey(key);
-    }
-
-    public String put(Long key, String value) {
-        return lexFileIdLexFileName.put(key, value);
-    }
-
-    public void putAll(Map<? extends Long, ? extends String> m) {
-        lexFileIdLexFileName.putAll(m);
-    }
-
-    public String remove(Object key) {
-        return lexFileIdLexFileName.remove(key);
-    }
-
-    public void clear() {
-        lexFileIdLexFileName.clear();
-    }
-
-    public boolean containsValue(Object value) {
-        return lexFileIdLexFileName.containsValue(value);
-    }
-
-    public Set<Long> keySet() {
-        return lexFileIdLexFileName.keySet();
-    }
-
-    public Collection<String> values() {
-        return lexFileIdLexFileName.values();
-    }
-
-    public Set<Map.Entry<Long, String>> entrySet() {
-        return lexFileIdLexFileName.entrySet();
     }
 }
--- extjwnl/src/main/java/net/sf/extjwnl/data/LexFileNameLexFileIdMap.java
+++ extjwnl/src/main/java/net/sf/extjwnl/data/LexFileNameLexFileIdMap.java
@@ -7,7 +7,7 @@ import java.util.*;
  *
  * @author <a rel="author" href="http://autayeu.com/">Aliaksandr Autayeu</a>
  */
-public abstract class LexFileNameLexFileIdMap implements Map<String, Long> {
+public abstract class LexFileNameLexFileIdMap {
 
     /**
      * A mapping of id's to files.
@@ -24,65 +24,5 @@ public abstract class LexFileNameLexFileIdMap implements Map<String, Long> {
 
     public static Map<String, Long> getMap() {
         return lexFileNameLexFileId;
-    }
-
-    public int size() {
-        return lexFileNameLexFileId.size();
-    }
-
-    public boolean equals(Object o) {
-        return lexFileNameLexFileId.equals(o);
-    }
-
-    public int hashCode() {
-        return lexFileNameLexFileId.hashCode();
-    }
-
-    public String toString() {
-        return lexFileNameLexFileId.toString();
-    }
-
-    public boolean isEmpty() {
-        return lexFileNameLexFileId.isEmpty();
-    }
-
-    public Long get(Object key) {
-        return lexFileNameLexFileId.get(key);
-    }
-
-    public boolean containsKey(Object key) {
-        return lexFileNameLexFileId.containsKey(key);
-    }
-
-    public Long put(String key, Long value) {
-        return lexFileNameLexFileId.put(key, value);
-    }
-
-    public void putAll(Map<? extends String, ? extends Long> m) {
-        lexFileNameLexFileId.putAll(m);
-    }
-
-    public Long remove(Object key) {
-        return lexFileNameLexFileId.remove(key);
-    }
-
-    public void clear() {
-        lexFileNameLexFileId.clear();
-    }
-
-    public boolean containsValue(Object value) {
-        return lexFileNameLexFileId.containsValue(value);
-    }
-
-    public Set<String> keySet() {
-        return lexFileNameLexFileId.keySet();
-    }
-
-    public Collection<Long> values() {
-        return lexFileNameLexFileId.values();
-    }
-
-    public Set<Map.Entry<String, Long>> entrySet() {
-        return lexFileNameLexFileId.entrySet();
     }
 }
